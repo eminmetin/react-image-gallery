7119213841fb7671c9a4eaeef0b27e5873de49f2
Muhammet Emin Metin <eminsteam58@hotmail.com>
2023-06-08 15:08:19 +0300

    images değişkene eklendi ve export edildi.

--- src/images.js
+++ src/images.js
@@ -1,7 +1,9 @@
-export default [
+ const images = [
   "https://pbs.twimg.com/media/EgYeSAlU0AAzizM?format=jpg&name=900x900",
   "https://i0.wp.com/www.thebarbell.com/wp-content/uploads/2023/01/Phil-Heath-Mr-Olympia-2.png?w=751&ssl=1",
   "https://e1.pxfuel.com/desktop-wallpaper/522/904/desktop-wallpaper-ronnie-coleman-bodybuilding-ronnie-coleman.jpg",
   "https://yt3.googleusercontent.com/ytc/AGIKgqNRrrvu6JB3iSptRiRPTZnOwhL9rRcjwV1osFsGaA=s900-c-k-c0x00ffffff-no-rj",
   "https://pbs.twimg.com/media/Ci8n4zEWgAAIoun?format=jpg&name=900x900",
 ];
+
+export default images
